bf0b2a23c6e536ce846bbc0816f40eae168eb8e8
unset <unset>
2026-01-14 09:57:41 +0800

    test

--- activities/backEnd/index.js
+++ activities/backEnd/index.js
@@ -3,6 +3,15 @@ import express from "express";
 const app = express();
 const PORT = 2999;
 
+app.get("/getName", (res, req) => {
+  var name = "Abelardo";
+  res.status(200).json(name);
+});
+
 app.listen(PORT, () => {
+  // GET -> fetch, name var name = "Abelardo"
+
+  // POST -> logic, if username = "Abelardo", password = "123" alert login success
+
   console.log(`Server is Running on PORT: ${PORT}`);
 });
