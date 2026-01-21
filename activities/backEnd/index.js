b92ccc5172ad0464ee53e263b022ce15182a3fd9
abel <abelardo.pascual@ciit.edu.ph>
2026-01-21 10:56:14 +0800

    activity work week3

--- activities/backEnd/index.js
+++ activities/backEnd/index.js
@@ -1,34 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "../backEnd/config/db.js";
 
-const app = express();
-const PORT = 3000;
+    const app = express();
+    const PORT = 3000;
 
-app.use(express.json());
+      dotenv.config();
+      app.use(express.json());
 
-app.get("/getName", (req, res) => {
-  var name = "Abelardo";
-  res.status(200).json(name);
-});
-
-app.post("/login", (req, res) => {
-  var {username, password} = req.body;
-  if (username == "Abelardo" && password == "123") {
-    res.status(200).json({
-      message: "Login successful.",
-      status: "Success"
-    })
-  } else {
-    res.status(403).json({
-      message: "Invalid username or password.",
-      status: "Failed"
-    })
-  }
-});
-
-app.listen(PORT, () => {
-  // GET -> fetch, name var name = "Abelardo"
-
-  // POST -> logic, if username = "Abelardo", password = "123" alert login success
-
-  console.log(`Server is Running on PORT: ${PORT}`);
-});
+      app.listen(PORT, () => {
+        // GET -> fetch, name var name = "Abelardo"
+        // POST -> logic, if username = "Abelardo", password = "123" alert login success
+        connectDB();
+        console.log(`Server is Running on PORT: ${PORT}`);
+      });
